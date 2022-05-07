b6f3e39ce9bbe52fc9e8a3c9e1a5812d37c6afee
ramon <2593406m@student.gla.ac.uk>
2022-05-07 09:22:41 +0100

    Initial commit, uploading files

---
+++ errors.py
@@ -0,0 +1,9 @@
+# exceptions
+
+
+# exception for required field being empty
+
+# exception for item not having enough info
+
+# exception for no data on item
+
---
+++ main.py
@@ -0,0 +1,36 @@
+# declare class to handle processing info
+
+# method to initialise data data
+# read csv data into pandas dataframe
+# call method to check for exceptions
+# Give user the option to call the method to delete problem rows (Separate method?)
+
+
+# Method to check for exceptions
+# Check for blank required fields (Separate method?)
+# if so raise exceptions
+# CHeck for any blank fields (Separate method?)
+# if so raise exceptions
+# Check for totally blank rows (Separate method?)
+# if so raise exceptions
+
+
+# Method to delete a number of rows when passed a list of them
+# delete the rows in the list passed as a parameter
+
+# Method to obtain list of names alphabetically
+# Slice dataframe so that it's only first and last names
+# join these in a new array
+# order and return array
+
+# Method to sort dataframe by date
+# sort dataframe by date
+# return dataframe
+
+# Method to get customer with latest check-in date
+# call method to sort dataframe by date
+# return last item
+
+# Method to get customer with earliest check-in date
+# call method to sort dataframe by date
+# return last item
